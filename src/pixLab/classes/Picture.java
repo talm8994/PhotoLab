35b40356f05a2ac4477c58b95170dd2ede36a8e4
talm8994 <tom.tom.pop13@gmail.com>
2016-01-20 13:47:39 -0700

    new methos

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -99,6 +99,32 @@ public class Picture extends SimplePicture
     }
   }
   
+  public void zeroRed()
+  {
+	  Pixel[][] redPixels = this.getPixels2D();
+	  for (int row = 0; row < redPixels.length; row++)
+	  {
+		  for(int col = 0; col < redPixels[0].length; col++)
+		  {
+			 Pixel currentPixel = redPixels[row][col];
+			 currentPixel.setRed(0);
+		  }
+	  }
+  }
+  
+  public void zeroGreen()
+  {
+	  Pixel[][] greenPixels = this.getPixels2D();
+	  for(int row = 0; row < greenPixels.length; row++)
+	  {
+		  for(int col = 0; col < greenPixels[0].length; col++)
+		  {
+			  Pixel currentPixel = greenPixels[row][col];
+			  currentPixel.setGreen(0);
+		  }
+	  }
+  }
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
@@ -224,10 +250,11 @@ public class Picture extends SimplePicture
    */
   public static void main(String[] args) 
   {
-    Picture beach = new Picture("beach.jpg");
+    Picture beach = new Picture("seagull.jpg");
     beach.explore();
-    beach.zeroBlue();
+    beach.zeroRed();
     beach.explore();
+    
   }
   
 } // this } is the end of class Picture, put all new methods before this
